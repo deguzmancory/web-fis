7c5b3c591f74daa6cffd1aed6c329e82e7a756bf
tin_pham <tin_pham@datahouse.com>
2023-01-19 00:36:41 +0700

    fix(Create User): hide delegation when create

--- src/containers/CRUUSerContainer/UserType/index.tsx
+++ src/containers/CRUUSerContainer/UserType/index.tsx
@@ -7,18 +7,21 @@ import ReceivedDelegation from './ReceivedDelegation';
 import SelectUserType from './SelectUserType';
 
 const UserType: React.FC<Props> = ({ formikProps }) => {
+  const { values } = formikProps;
   return (
     <Box>
       <Typography variant="h3">User Type</Typography>
       <Box my={3}>
         <SelectUserType formikProps={formikProps} />
       </Box>
-      <Box>
-        <Accordion title={'Delegation'}>
-          <GrantDelegation formikProps={formikProps} />
-          <ReceivedDelegation formikProps={formikProps} />
-        </Accordion>
-      </Box>
+      {values.isViewMode && (
+        <Box>
+          <Accordion title={'Delegation'}>
+            <GrantDelegation formikProps={formikProps} />
+            <ReceivedDelegation formikProps={formikProps} />
+          </Accordion>
+        </Box>
+      )}
     </Box>
   );
 };
