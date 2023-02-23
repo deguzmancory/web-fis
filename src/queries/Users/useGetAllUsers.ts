08692c7d1339c3b7ec8ee307682fa79534bea95a
tin_pham <tin_pham@datahouse.com>
2023-02-23 11:10:48 +0700

    fix(useGetAllUSers): fix type response

--- src/queries/Users/useGetAllUsers.ts
+++ src/queries/Users/useGetAllUsers.ts
@@ -7,7 +7,11 @@ import { API_QUERIES } from '../keys';
 import { GetPropertiesParams, User } from './types';
 
 export function useGetAllUsers(
-  options?: UseQueryOptions<ApiResponseType<PaginationResponseType<User>>, Error>
+  options?: UseQueryOptions<
+    ApiResponseType<PaginationResponseType<User>>,
+    Error,
+    PaginationResponseType<User>
+  >
 ) {
   const [params, setParams] = useState<GetPropertiesParams>({});
   const {
@@ -16,30 +20,29 @@ export function useGetAllUsers(
     isError,
     isFetching,
     refetch: onGetAllUsers,
-  } = useQuery<ApiResponseType<PaginationResponseType<User>>, Error>([API_QUERIES.USERS, params], {
-    queryFn: (query) => {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const [_, ...params] = query.queryKey;
-      return responseWrapper<ApiResponseType<PaginationResponseType<User>>>(
-        apiClient.getAllUsers,
-        params
-      );
-    },
-    select: (data) => {
-      return data;
-    },
-    notifyOnChangeProps: ['data', 'isFetching'],
-    keepPreviousData: true,
-    enabled: !isEmpty(params),
-    ...options,
-  });
+  } = useQuery<ApiResponseType<PaginationResponseType<User>>, Error, PaginationResponseType<User>>(
+    [API_QUERIES.USERS, params],
+    {
+      queryFn: (query) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const [_, ...params] = query.queryKey;
+        return responseWrapper<ApiResponseType<PaginationResponseType<User>>>(
+          apiClient.getAllUsers,
+          params
+        );
+      },
+      notifyOnChangeProps: ['data', 'isFetching'],
+      keepPreviousData: true,
+      enabled: !isEmpty(params),
+      ...options,
+    }
+  );
 
   const queryClient = useQueryClient();
 
   const handleInvalidateAllUser = () => queryClient.invalidateQueries(API_QUERIES.USERS);
 
-  const { data } = allUsersResponse || {};
-  const { data: users, hasNext, payloadSize, totalRecords } = data || {};
+  const { data: users, hasNext, payloadSize, totalRecords } = allUsersResponse || {};
 
   return {
     users,
