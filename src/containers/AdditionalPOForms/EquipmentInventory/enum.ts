b5e60663ccea4f26e86b80a5608a1f5f8759bee5
deguzmancoryDH <cory_deguzman@datahouse.com>
2023-03-13 17:47:51 -0700

    feat: Equipment Inventory

--- src/containers/AdditionalPOForms/EquipmentInventory/enum.ts
+++ src/containers/AdditionalPOForms/EquipmentInventory/enum.ts
@@ -1,3 +1,38 @@
 export enum PO_EQUIPMENT_INVENTORY_FORM_KEY {
-  DESCRIPTION_OF_EQUIPMENT = 'descriptionOfEquipment',
+  EQUIPMENT_DESCRIPTION = 'equipmentDescription',
+  EQUIPMENT_LOCATION = 'equipmentLocation',
+  BUILDING_CODE = 'buildingCode',
+  DECAL = 'decal',
+  DECAL2 = 'decal2',
+  PART_OF_FABRICATION = 'Component is part of a Fabrication - an item that is to become part of something being built.',
+  FINISHED_PRODUCT_NAME = 'finishedProductName',
+  COMPLETED_BY = 'completedBy',
+  EQUIPMENT_TYPE = 'equipmentType',
+}
+
+export enum EQUIPMENT_TYPE {
+  STAND_ALONE = 'standAlone',
+  INTERCHANGEABLE = 'interchangeable',
+  COMPONENT = 'component',
+}
+
+export const optionCheckboxValue = [
+  {
+    label:
+      'Stand-alone - defined as equipment which is $5000 and above and has a life expectancy of at least 1 year.',
+    value: EQUIPMENT_TYPE.STAND_ALONE,
+  },
+  {
+    label:
+      'Interchangeable Equipment - defined as equipment which is $5000 and above and has a life expectancy of at least 1 year but can be used interchangeably with other equipment and cannot function by itself.',
+    value: EQUIPMENT_TYPE.INTERCHANGEABLE,
+  },
+  {
+    label: 'Component - an item that becomes a part of the existing equipment.',
+    value: EQUIPMENT_TYPE.COMPONENT,
+  },
+];
+
+export enum EQUIPMENT_INVENTORY_LABEL {
+  PART_OF_FABRICATION = 'Component is part of a Fabrication - an item that is to become part of something being built.',
 }
--- src/containers/AdditionalPOForms/EquipmentInventory/index.tsx
+++ src/containers/AdditionalPOForms/EquipmentInventory/index.tsx
@@ -1,26 +1,20 @@
-import {
-  Box,
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  Grid,
-  RadioGroup,
-} from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import { FormikProps, useFormik } from 'formik';
 import React, { RefObject } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { PATHS } from 'src/appConfig/paths';
-import { Input, TextArea } from 'src/components/common';
+import { Checkbox, Input, TextareaAutosize } from 'src/components/common';
 import { UpsertPOFormValue } from 'src/containers/PurchaseOrderContainer/types';
-import { POSoleSourcePayload } from 'src/queries/PurchaseOrders';
+import { POEquipmentInventoryPayload } from 'src/queries/PurchaseOrders';
 import { setFormData } from 'src/redux/form/formSlice';
 import { IRootState } from 'src/redux/rootReducer';
 import { Navigator } from 'src/services';
 import { getErrorMessage, getUncontrolledInputFieldProps } from 'src/utils';
-import { PO_EQUIPMENT_INVENTORY_FORM_KEY } from './enum';
-import { PO_FORM_KEY } from 'src/containers/PurchaseOrderContainer/enums';
+import { PO_EQUIPMENT_INVENTORY_FORM_KEY, optionCheckboxValue } from './enum';
+import { PO_ADDITIONAL_FORM_KEY, PO_ADDITIONAL_FORM_PARAMS } from '../enum';
+import { initialEquipmentInventoryValue } from 'src/containers/PurchaseOrderContainer/constants';
+import Layout from 'src/containers/CRUUSerContainer/layout';
 
 const EquipmentInventoryForm: React.FC<Props> = ({
   formRef,
@@ -32,21 +26,31 @@ const EquipmentInventoryForm: React.FC<Props> = ({
 
   const handleFormSubmit = () => {
     handleSaveForm();
-    Navigator.navigate(PATHS.createPurchaseOrders);
+    Navigator.navigate(
+      `${PATHS.createPurchaseOrders}?${PO_ADDITIONAL_FORM_PARAMS.SCROLL_TO}=${PO_ADDITIONAL_FORM_KEY.ADDITIONAL_FORMS}`
+    );
   };
 
-  const formik = useFormik<POSoleSourcePayload>({
-    initialValues: formData?.soleSource,
+  const handleResetForm = () => {
+    onSetFormData<UpsertPOFormValue>({
+      ...formData,
+      equipmentInventory: initialEquipmentInventoryValue,
+    });
+  };
+
+  const formik = useFormik<POEquipmentInventoryPayload>({
+    initialValues: formData?.equipmentInventory || initialEquipmentInventoryValue,
     validationSchema: null,
     enableReinitialize: true,
     onSubmit: handleFormSubmit,
+    onReset: handleResetForm,
   });
 
   React.useImperativeHandle(formRef, () => ({
     ...formik,
   }));
 
-  const { values, errors, touched, setFieldValue, setFieldTouched } = formik;
+  const { values, errors, touched, setFieldValue, setFieldTouched, getFieldProps } = formik;
 
   const _getUncontrolledFieldProps = getUncontrolledInputFieldProps({
     values,
@@ -54,12 +58,8 @@ const EquipmentInventoryForm: React.FC<Props> = ({
     setFieldValue,
   });
 
-  // const _handleScrollToTopError = React.useCallback(() => {
-  //   handleScrollToTopError(errors);
-  // }, [errors]);
-
   const handleSaveForm = React.useCallback(() => {
-    onSetFormData<UpsertPOFormValue>({ ...formData, soleSource: values });
+    onSetFormData<UpsertPOFormValue>({ ...formData, equipmentInventory: values });
   }, [formData, onSetFormData, values]);
 
   React.useEffect(() => {
@@ -76,81 +76,132 @@ const EquipmentInventoryForm: React.FC<Props> = ({
 
   return (
     <Box>
-      <Grid container spacing={2}>
-        <Grid item container spacing={3}>
-          <Grid item xs={12} sm={6} md={4}>
-            <Input
-              label={'Purchase Order No'}
-              {..._getUncontrolledFieldProps(PO_FORM_KEY.NUMBER)}
-              placeholder={'To be assigned'}
-              disabled
-            />
+      <Container maxWidth="lg">
+        <Layout>
+          <Grid container spacing={2}>
+            <Grid item container spacing={3}>
+              <Grid item xs={12} sm={6} md={4}>
+                <Input label={'Purchase Order No'} value={formData?.number} name="" disabled />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  1. DESCRIPTION OF EQUIPMENT (provide a generic name describing the equipment and
+                  provide the manufacturer and model no.)
+                </Typography>
+                <TextareaAutosize
+                  errorMessage={_getErrorMessage(
+                    PO_EQUIPMENT_INVENTORY_FORM_KEY.EQUIPMENT_DESCRIPTION
+                  )}
+                  {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.EQUIPMENT_DESCRIPTION)}
+                />
+              </Grid>
+
+              <Grid item xs={2.29} className="justify-flex-end">
+                <Typography variant="body1">2. Indicate Building Code</Typography>
+              </Grid>
+              <Grid item xs={3} sx={{ marginLeft: -2 }}>
+                <Input
+                  errorMessage={_getErrorMessage(PO_EQUIPMENT_INVENTORY_FORM_KEY.BUILDING_CODE)}
+                  {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.BUILDING_CODE)}
+                  disabled={disabled}
+                />
+              </Grid>
+              <Grid item xs={6.6} className="justify-flex-end" sx={{ marginLeft: -3 }}>
+                <Typography variant="body1">
+                  (search for Building code in UH KFS Maintenance tab, System section, Building
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sx={{ marginTop: -2 }}>
+                <Typography variant="body1">
+                  lookup) or address of where the equipment is to be located:
+                </Typography>
+                <TextareaAutosize
+                  errorMessage={_getErrorMessage(
+                    PO_EQUIPMENT_INVENTORY_FORM_KEY.EQUIPMENT_LOCATION
+                  )}
+                  {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.EQUIPMENT_LOCATION)}
+                />
+              </Grid>
+            </Grid>
+            <Grid item container>
+              <Grid item xs={12}>
+                <Typography sx={{ paddingBottom: 1 }} variant="body1">
+                  3. Please check one of the following:
+                </Typography>
+                <Checkbox.Group
+                  columns={1}
+                  options={optionCheckboxValue}
+                  errorMessage={_getErrorMessage(PO_EQUIPMENT_INVENTORY_FORM_KEY.EQUIPMENT_TYPE)}
+                  {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.EQUIPMENT_TYPE)}
+                  disabled={disabled}
+                  onChange={setFieldValue}
+                />
+                <Grid item container sx={{ px: 2 }}>
+                  <Grid item xs={6} sx={{ mb: 1 }}>
+                    <Input
+                      label={'Please indicate Decal or P.O. No. for component to be incorporated.'}
+                      errorMessage={_getErrorMessage(PO_EQUIPMENT_INVENTORY_FORM_KEY.DECAL)}
+                      {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.DECAL)}
+                      disabled={disabled}
+                    />
+                  </Grid>
+                  <Checkbox.Item
+                    label={
+                      'Component is part of a Fabrication - an item that is to become part of something being built.'
+                    }
+                    {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.PART_OF_FABRICATION)}
+                    errorMessage={_getErrorMessage(
+                      PO_EQUIPMENT_INVENTORY_FORM_KEY.PART_OF_FABRICATION
+                    )}
+                    disabled={disabled}
+                  />
+                  <Grid item container spacing={2} sx={{ py: 1 }}>
+                    <Grid item xs={6}>
+                      <Input
+                        label={'Please provide the name of the finished product.'}
+                        errorMessage={_getErrorMessage(
+                          PO_EQUIPMENT_INVENTORY_FORM_KEY.FINISHED_PRODUCT_NAME
+                        )}
+                        {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.FINISHED_PRODUCT_NAME)}
+                        disabled={disabled}
+                      />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Input
+                        label={
+                          'Please indicate Decal or P.O. No. for component to be incorporated.'
+                        }
+                        errorMessage={_getErrorMessage(PO_EQUIPMENT_INVENTORY_FORM_KEY.DECAL2)}
+                        {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.DECAL2)}
+                        disabled={disabled}
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
           <Grid item xs={12}>
-            <TextArea
-              label={
-                '1. DESCRIPTION OF EQUIPMENT (provide a generic name describing the equipment and provide the manufacturer and model no.)'
-              }
-              errorMessage={_getErrorMessage(
-                PO_EQUIPMENT_INVENTORY_FORM_KEY.DESCRIPTION_OF_EQUIPMENT
-              )}
-            />
+            <Typography variant="body2">
+              NOTE: A FORM MUST BE SUBMITTED FOR EACH PIECE OF EQUIPMENT TO BE ISSUED A DECAL.
+              FAILURE TO COMPLETE THIS FORM AND ATTACH TO ALL EQUIPMENT PURCHASE ORDERS WILL DELAY
+              PROCESSING.
+            </Typography>
           </Grid>
-          <Grid item sm={12}>
-            <TextArea
-              label={
-                '2. Indicate Building code (search for Building Code in UH KFS Maintenance tab, System Section, Building lookup) or address of where the equipment is to be located'
-              }
-              errorMessage={_getErrorMessage(
-                PO_EQUIPMENT_INVENTORY_FORM_KEY.DESCRIPTION_OF_EQUIPMENT
-              )}
-            />
-          </Grid>
-          <Grid item sm={12}>
-            <FormControl>
-              <FormLabel id="equipment-inventory">Please select one of the following:</FormLabel>
-              <RadioGroup aria-labelledby="equipment-inventory" name="equipment-inventory-group">
-                <FormControlLabel
-                  value="stand-alone"
-                  control={<Checkbox />}
-                  label="Stand-alone - defined as equipment which is $5000 and above and has a life expectancy of at least 1 year."
-                />
-                <FormControlLabel
-                  value="interchangeable-equipment"
-                  control={<Checkbox />}
-                  label="Interchangeable Equipment - defined as equipment which is $5000 and above and has a life expectancy of 1 year but can be used interchangeably with other equipment and cannot function by itself."
-                />
-                <FormControlLabel
-                  value="component"
-                  control={<Checkbox />}
-                  label="Component - an item that becomes a part of the existing equipment."
-                />
-              </RadioGroup>
-              <Grid item sm={6} sx={{ paddingLeft: 4 }}>
-                <Input
-                  label={'Please indicate Decal or P.O. No. for component to be incorporated.'}
-                />
-              </Grid>
-              <FormControlLabel
-                sx={{ paddingLeft: 4 }}
-                value="fabrication"
-                control={<Checkbox />}
-                label="Component is part of a Fabrication - an item that is to become part of something being built."
+        </Layout>
+        <Layout>
+          <Grid item container spacing={2}>
+            <Grid item xs={6}>
+              <Input
+                label={'Form Completed By'}
+                errorMessage={_getErrorMessage(PO_EQUIPMENT_INVENTORY_FORM_KEY.COMPLETED_BY)}
+                {...getFieldProps(PO_EQUIPMENT_INVENTORY_FORM_KEY.COMPLETED_BY)}
+                disabled={disabled}
               />
-              <Grid container spacing={3} sx={{ paddingLeft: 4 }}>
-                <Grid item sm={6}>
-                  <Input label={'Please provide the name of the finished product.'} />
-                </Grid>
-                <Grid item sm={6}>
-                  <Input
-                    label={'Please indicate Decal or P.O No. for component to be incorporated. '}
-                  />
-                </Grid>
-              </Grid>
-            </FormControl>
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
+        </Layout>
+      </Container>
     </Box>
   );
 };
